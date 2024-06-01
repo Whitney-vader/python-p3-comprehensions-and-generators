9bf381e426a1823c5e60d81dd11930a689c85019
Whitney_vader <whitney@gmail.com>
2024-06-01 18:56:06 +0300

    Initial commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+def return_evens(seq):
+    return [x for x in seq if x % 2 == 0]
+print(return_evens([0, 1, 3, 5, 7, 8, 9])) 
 
-def return_evens(num_list):
-    pass
-
-def make_exclamation(sentence_list):
-    pass
+def make_exclamation(sentences):
+    return [sentence + "!" for sentence in sentences]
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))  
